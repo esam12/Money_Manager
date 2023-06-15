ca8eb1eb00254c7a2d3aca4bbc68d28a9be0ee0c
mobarmeg.9@gmail.com <alzeabiesam@gmail.com>
2023-06-15 23:45:05 +0300

    15.06.2023

---
+++ lib/view/screens/home_screen_tab.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenTab extends StatelessWidget {
+  const HomeScreenTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Home"),
+      ),
+    );
+  }
+}
--- lib/view/screens/main_screen_host.dart
+++ lib/view/screens/main_screen_host.dart
@@ -1,30 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:money_manager/utils/constants.dart';
+import 'package:money_manager/view/screens/home_profile_tab.dart';
+import 'package:money_manager/view/screens/home_screen_tab.dart';
 
-class MainScreenHost extends StatelessWidget {
+class MainScreenHost extends StatefulWidget {
   const MainScreenHost({super.key});
 
-  buildContent(currentIndex) {}
+  @override
+  State<MainScreenHost> createState() => _MainScreenHostState();
+}
+
+class _MainScreenHostState extends State<MainScreenHost> {
+  int currentIndex = 0;
+
+  Widget buildContent(int index) {
+    switch (index) {
+      case 0:
+        return const HomeScreenTab();
+      case 1:
+        return Container();
+      case 2:
+        return Container();
+      case 3:
+        return const HomeProfileTab();
+      default:
+        return const HomeScreenTab();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: buildContent(currentIndex),
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+          print(currentIndex);
+        },
+        selectedItemColor: secondaryDark,
+        unselectedItemColor: fontLight,
         items: [
           BottomNavigationBarItem(
-            icon: Image.asset('assets/icons/home-1.png'),
+            icon: Image.asset(
+              'assets/icons/home-1.png',
+            ),
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Image.asset('assets/icons/home-1.png'),
-            label: 'Home',
+            icon: Image.asset(
+              'assets/icons/chart-vertical.png',
+            ),
+            label: 'Stat',
           ),
           BottomNavigationBarItem(
-            icon: Image.asset('assets/icons/home-1.png'),
-            label: 'Home',
+            icon: Image.asset(
+              'assets/icons/wallet.png',
+            ),
+            label: 'Wallet',
           ),
           BottomNavigationBarItem(
-            icon: Image.asset('assets/icons/user-1.png'),
+            icon: Image.asset(
+              'assets/icons/user-1.png',
+            ),
             label: 'Profile',
           )
         ],
